2648f8430dbc38699e161819f360d3ab28812ff4
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-10-28 15:43:22 +0800

    dsa

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -12,11 +12,8 @@ const Auth = () => {
                         <h2 className="text-white text-4xl mb-8 font-semibold">Sign in</h2>
                         <div className="flex flex-col gap-4">
                             <Input />
-
                         </div>
-
                     </div>
-
                 </div>
             </div>
         </div>
