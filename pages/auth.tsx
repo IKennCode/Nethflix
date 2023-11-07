9b9d08c8fe871a52cba923cfbc7a813254d33678
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-11-07 21:19:21 +0800

    NEW ENV

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,19 +1,49 @@
 import Input from "@/component/input";
 import {useCallback, useState} from "react";
 import axios from "axios";
+import {signIn} from "next-auth/react";
+import { useRouter } from "next/router";
+import {FcGoogle} from 'react-icons/fc';
+import {ImGithub} from 'react-icons/im'
+
 
 const Auth = () => {
 
+    //declared router after an await for login
+    const router = useRouter();
+
+    // useState for Login and Register
     const [email, setEmail] = useState('');
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
 
+    //variant for login or register
     const [variant, setVariant] = useState('');
 
     const toggleVariant = useCallback (() => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
     }, []);
 
+     //login authentication
+     const login = useCallback(async () => {
+        try {
+            await signIn('credentials', {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+
+        }catch (error) {
+            console.log(error);
+        }
+
+    }, [email, password, router]);
+
+
+    //register authentication
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -22,11 +52,15 @@ const Auth = () => {
                 password
             });
 
+            login();
+
         }catch (error) {
             console.log(error);
         }
 
-    }, [email, name, password]);
+    }, [email, name, password, login]);
+
+   
 
 
 
@@ -46,7 +80,16 @@ const Auth = () => {
 
                             <Input label="Email" onChange={(ev: any) => setEmail(ev.target.value)} id="email" type="email" value={email} />
                             <Input label="Password" onChange={(ev:any) => setPassword(ev.target.value)} id="password" type="password" value={password} />
-                            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign Up'}</button>
+                            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign Up'}</button>
+                            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+                                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <FcGoogle size={30}/>
+                                </div>
+                                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                                    <ImGithub size={30}/>
+                                </div>
+                            </div>
+                            
                             <p className="text-neutral-500 mt-12"> 
                                 {variant === 'login' ? 'First Time using Netflix?' : 'Already have an Account?'}
                             <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
