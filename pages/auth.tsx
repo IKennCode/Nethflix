326d6b5283f8c771d4a4715928bd4b4b60c84c0d
Kenneth Dimalibot <dimalibotkennethofficial@gmail.com>
2023-11-08 21:51:46 +0800

    das

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -65,42 +65,98 @@ const Auth = () => {
 
 
     return (
-        <div className="relative h-full w-full bg-[url('/images/nethflix.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
-            <div className=" bg-black w-full h-full lg:bg-opacity-40">
-                <nav className=" px-12 py-5">
-                    <img src="/images/logo.png" alt="Logo" className="h-12 "></img>
-                </nav>
-                <div className=" flex justify-center">
-                    <div className=" bg-black bg-opacity-70 px-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
-                        <h2 className="text-white text-4xl mb-8 font-semibold">{variant === 'login' ? 'Sign in' :'Register'}</h2>
-                        <div className="flex flex-col gap-4">
-                            {variant === 'register' && (
-                            <Input label="Username" onChange={(ev: any) => setName(ev.target.value)} id="name" type="email" value={name} />
-                            )}
+        // <div className="relative h-full w-full bg-[url('/images/nethflix.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        //     <div className=" bg-black w-full h-full lg:bg-opacity-40">
+        //         <nav className=" px-12 py-5">
+        //             <img src="/images/logo.png" alt="Logo" className="h-12 "></img>
+        //         </nav>
+        //         <div className=" flex justify-center">
+        //             <div className=" bg-black bg-opacity-70 px-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+        //                 <h2 className="text-white text-4xl mb-8 font-semibold">{variant === 'login' ? 'Sign in' :'Register'}</h2>
+        //                 <div className="flex flex-col gap-4">
+        //                     {variant === 'register' && (
+        //                     <Input label="Username" onChange={(ev: any) => setName(ev.target.value)} id="name" type="email" value={name} />
+        //                     )}
 
-                            <Input label="Email" onChange={(ev: any) => setEmail(ev.target.value)} id="email" type="email" value={email} />
-                            <Input label="Password" onChange={(ev:any) => setPassword(ev.target.value)} id="password" type="password" value={password} />
-                            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign Up'}</button>
-                            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
-                                <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                                    <FcGoogle size={30}/>
-                                </div>
-                                <div onClick={() => signIn('github', {callbackUrl: '/'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
-                                    <ImGithub size={30}/>
-                                </div>
-                            </div>
+        //                     <Input label="Email" onChange={(ev: any) => setEmail(ev.target.value)} id="email" type="email" value={email} />
+        //                     <Input label="Password" onChange={(ev:any) => setPassword(ev.target.value)} id="password" type="password" value={password} />
+        //                     <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign Up'}</button>
+        //                     <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+        //                         <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+        //                             <FcGoogle size={30}/>
+        //                         </div>
+        //                         <div onClick={() => signIn('github', {callbackUrl: '/'})} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+        //                             <ImGithub size={30}/>
+        //                         </div>
+        //                     </div>
                             
-                            <p className="text-neutral-500 mt-12"> 
-                                {variant === 'login' ? 'First Time using Netflix?' : 'Already have an Account?'}
-                            <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
-                                {variant === 'login' ? 'Create an Account ' : 'Login'}</span>
-                            </p>
+        //                     <p className="text-neutral-500 mt-12"> 
+        //                         {variant === 'login' ? 'First Time using Netflix?' : 'Already have an Account?'}
+        //                     <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+        //                         {variant === 'login' ? 'Create an Account ' : 'Login'}</span>
+        //                     </p>
 
-                        </div>
-                    </div>
-                </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="relative h-full w-full bg-[url('/images/nethflix.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
+        </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">
+              {variant === 'login' ? 'Sign in' : 'Register'}
+            </h2>
+            <div className="flex flex-col gap-4">
+              {variant === 'register' && (
+                <Input
+                  id="name"
+                  type="text"
+                  label="Username"
+                  value={name}
+                  onChange={(e: any) => setName(e.target.value)} 
+                />
+              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)} 
+              />
+              <Input
+                type="password" 
+                id="password" 
+                label="Password" 
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)} 
+              />
             </div>
+            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div onClick={() => signIn('google', { callbackUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={32} />
+              </div>
+              <div onClick={() => signIn('github', { callbac2kUrl: '/profiles' })} className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <ImGithub size={32} />
+              </div>
+            </div>
+            <p className="text-neutral-500 mt-12">
+              {variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
+              <span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">
+                {variant === 'login' ? 'Create an account' : 'Login'}
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
+    </div>
     );
 }
 
